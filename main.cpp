b417cdddb4f65a75ddb925b5581c8f6851f4a582
QuinMeow <nicopoiduang5228@gmail.com>
2021-12-25 17:15:59 +0800

    主界面构建

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <graphics.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int operate = 0;
+	while (operate != 4)
+	{
+
+		cout << "1----Load Document" << endl
+			<< "2----Count a word" << endl
+			<< "3----Locate a word" << endl
+			<< "4----Exit" << endl
+			<< "Choose Your Operation:";
+		cin >> operate;
+		switch (operate)
+		{
+		case 1:
+			cout << "Load Document" << endl;
+			break;
+		case 2:
+			cout << "Count a word" << endl;
+			break;
+		case 3:
+			cout << "Locate a word" << endl;
+			break;
+		case 4:
+			return 1;
+		default:
+			cout << "Wrong Operation" << endl;
+			break;
+		}
+	}
+
+}
